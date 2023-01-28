1a9020b5a062ae9122c39229e0b2114741c8729c
Diego Fasolo <diegofaso00@gmail.com>
2023-01-28 12:50:53 +0100

    Commit update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { Mesh } from 'three'
+import viteLogo from '/vite.svg'
+import reactLogo from './assets/react.svg'
+import threejsLogo from './assets/threejs.svg'
 import './App.css'
 
 function Box() {
@@ -36,6 +39,14 @@ function App() {
 
   return (
     <div className="App">
+      <div className='flex flex-col justify-center items-center gap-4 mt-10'>
+        <h1 className='font-bold text-transparent text-5xl bg-clip-text bg-gradient-to-r from-yellow-400 via-yellow-200 to-yellow-400'>Vite + React-Ts-3D</h1>
+        <div className='flex justify-center items-center gap-4'>
+          <img src={viteLogo} className='h-12 w-12' alt='vite' />
+          <img src={reactLogo} className='h-12 w-12' alt='react' />
+          <img src={threejsLogo} className='h-12 w-12' alt='threejs' />
+        </div>
+      </div>
       <ThreeScene />
     </div>
   )
